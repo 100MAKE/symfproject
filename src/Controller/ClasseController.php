3abaeb06f1b30f0f9fa8f38ed8d8c16a434cbb7f
SIDY SAMAKE SAMBOU <samakesamboudysi@gmail.com>
2022-06-01 18:43:32 +0000

    second commit

--- src/Controller/ClasseController.php
+++ src/Controller/ClasseController.php
@@ -3,17 +3,22 @@
 namespace App\Controller;
 
 use App\Repository\ClasseRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ClasseController extends AbstractController
 {
     #[Route('/classe', name: 'app_classe')]
-    public function index(ClasseRepository $repo): Response
+    public function index(ClasseRepository $repo,
+    PaginatorInterface $pagination,
+    Request $request): Response
     {    $repo->findAll();
         //  dd($repo->findAll());
         $last= $repo->findAll();
+        $last=$pagination->paginate($last,$request->query->getInt('page',1),5);
         
         return $this->render('classe/index.html.twig', [
             'controller_name' => 'ClasseController',
